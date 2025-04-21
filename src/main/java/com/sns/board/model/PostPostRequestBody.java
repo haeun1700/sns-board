53801305d87c3ae708f22263bb388123efb646d9
haeun1700 <haeun7173@gmail.com>
2025-04-21 15:53:33 +0900

    #1: record 형식으로 변환 - record형식으로 변환한다면 단순히 데이터를 전달하는 PostPostRequestBody에 들어갈 getter/setter/tostring등 을 record가 대신할 수 있다.

--- src/main/java/com/sns/board/model/PostPostRequestBody.java
+++ src/main/java/com/sns/board/model/PostPostRequestBody.java
@@ -1,32 +1,3 @@
 package com.sns.board.model;
 
-import java.util.Objects;
-
-public class PostPostRequestBody {
-    private String body;
-
-    public PostPostRequestBody(String body) {
-        this.body = body;
-    }
-    public PostPostRequestBody() {}
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof PostPostRequestBody that)) return false;
-        return Objects.equals(body, that.body);
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hashCode(body);
-    }
-
-    public String getBody() {
-        return body;
-    }
-
-    public void setBody(String body) {
-        this.body = body;
-    }
-}
+public record PostPostRequestBody(String body){}
