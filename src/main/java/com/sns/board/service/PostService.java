c61e0ad0c665b8124b0c4337d6a4e56f0885a4a4
haeun1700 <haeun7173@gmail.com>
2025-04-21 15:44:11 +0900

    #1:post Service 구현 - 반복되는 데이터를 static에 구성 - 단건 조회: Optional로 감싸, map의 반환 조건을 편리하게 구성한다. -createPost(): id가 1씩 증가하도록 구성하고 싶기에, 현재 id의 max값을 가져온 후 여기에 1씩 추가한다.

---
+++ src/main/java/com/sns/board/service/PostService.java
@@ -0,0 +1,37 @@
+package com.sns.board.service;
+
+import com.sns.board.model.Post;
+import com.sns.board.model.PostPostRequestBody;
+import org.springframework.stereotype.Service;
+
+import java.time.ZonedDateTime;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PostService {
+    private static final List<Post> posts = new ArrayList<>();
+
+    static {
+        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
+        posts.add(new Post(2L, "Post 2", ZonedDateTime.now()));
+        posts.add(new Post(3L, "Post 3", ZonedDateTime.now()));
+    }
+
+    public List<Post> getPosts() {
+        return posts;
+    }
+    public Optional<Post> getPost(Long id) {
+        return posts.stream().filter(post -> id.equals(post.getPostId())).findFirst();
+    }
+
+    public Post createPost(PostPostRequestBody postPostRequestBody) {
+        Long newPostId = posts.stream().mapToLong(Post::getPostId).max().orElse(0L)+1;
+
+        Post newPost = new Post(newPostId, postPostRequestBody.getBody(), ZonedDateTime.now());
+        posts.add(newPost);
+
+        return newPost;
+    }
+}
