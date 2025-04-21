282945dbfa528044375fb5313db090e878a4e162
haeun1700 <haeun7173@gmail.com>
2025-04-21 15:48:48 +0900

    #1: PostController 재구성 - postService를 통해 구성된 데이터 값을 의존성 주입을 통해 사용 - @Requestbody를 통해 새롭게 작성한 Post를 생성한다.

--- src/main/java/com/sns/board/controller/PostController.java
+++ src/main/java/com/sns/board/controller/PostController.java
@@ -1,46 +1,42 @@
 package com.sns.board.controller;
 
 import com.sns.board.model.Post;
-import org.springframework.http.HttpStatus;
+import com.sns.board.model.PostPostRequestBody;
+import com.sns.board.service.PostService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
-
-import java.time.ZonedDateTime;
-import java.util.ArrayList;
+import org.springframework.web.bind.annotation.*;
 import java.util.List;
 import java.util.Optional;
 
 @RestController
+@RequestMapping("/api/v1/posts")
 public class PostController {
 
-    @GetMapping("/api/v1/posts")
-    public ResponseEntity<List<Post>> getPosts(){
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
-        posts.add(new Post(2L, "Post 1", ZonedDateTime.now()));
-        posts.add(new Post(3L, "Post 1", ZonedDateTime.now()));
+    @Autowired
+    private PostService postService; // 의존성 주입으로 객체 생성을 스프링에게 맡긴다.
 
-        return new ResponseEntity<>(posts, HttpStatus.OK); //response는 json 리스트로 반환
+    @GetMapping
+    public ResponseEntity<List<Post>> getPosts(){
+        List<Post> posts = postService.getPosts();
+        return ResponseEntity.ok(posts); //response는 json 리스트로 반환
     }
     /*
     * Response entity: 응답 본문 + 상태코드+헤더
+    * Optional: 특정 게시글 or null 반환 -> 단건 map()이 있어 쉽게 반환 형태 가능
+    * 만약 List가 반환 타입이라면 []이 반환된다. ->단건 map()이 없어 get(0)을 추가해야함.
      */
-    @GetMapping("/api/v1/posts/{id}")
+    @GetMapping("/{id}")
     public ResponseEntity<Post> getPost(@PathVariable Long id){
-        List<Post> posts = new ArrayList<>();
-        posts.add(new Post(1L, "Post 1", ZonedDateTime.now()));
-        posts.add(new Post(2L, "Post 2", ZonedDateTime.now()));
-        posts.add(new Post(3L, "Post 3", ZonedDateTime.now()));
-
-
-        Optional<Post> matchingPost = posts.stream()
-                .filter(post -> id.equals(post.getPostId())) //조건에 맞는 post만 남김
-                .findFirst(); // 그중 첫번째만 반환
-
+        Optional<Post> matchingPost = postService.getPost(id);
         return matchingPost
                 .map(ResponseEntity::ok) //값이 있으면 ok반환
                 .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Post> createPost(@RequestBody PostPostRequestBody postPostRequestBody){
+        Post post = postService.createPost(postPostRequestBody);
+        return ResponseEntity.ok(post);
     }
 }
