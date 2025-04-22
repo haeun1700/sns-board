3114a2283583abfd6f58570a4b1d5802176656d6
haeun1700 <haeun7173@gmail.com>
2025-04-22 12:45:06 +0900

    #1: record에서 class로 Post변경 - record는 불변객체이므로 수정이 불가능하다. 따라서 update는 setter를 통해 수정이 이뤄져야하기에, 실제 정보를 담는 곳은 class로, 단순히 데이터 전송만 하는 객체는 record형식으로 둔다.

--- src/main/java/com/sns/board/model/Post.java
+++ src/main/java/com/sns/board/model/Post.java
@@ -1,6 +1,42 @@
 package com.sns.board.model;
 
+import lombok.Getter;
+import lombok.Setter;
+
 import java.time.ZonedDateTime;
 import java.util.Objects;
 
-public record Post(Long postId, String body, ZonedDateTime createAt){}
+@Getter
+@Setter
+public class Post{
+    private Long id;
+    private String body;
+    private ZonedDateTime createdAt;
+
+    public Post(long id, String body, ZonedDateTime createdAt) {
+        this.id = id;
+        this.body = body;
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Post post)) return false;
+        return Objects.equals(getId(), post.getId()) && Objects.equals(getBody(), post.getBody()) && Objects.equals(getCreatedAt(), post.getCreatedAt());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getId(), getBody(), getCreatedAt());
+    }
+
+    @Override
+    public String toString() {
+        return "Post{" +
+                "id=" + id +
+                ", body='" + body + '\'' +
+                ", createdAt=" + createdAt +
+                '}';
+    }
+}
