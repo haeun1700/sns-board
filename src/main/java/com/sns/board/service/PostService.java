c5272360a8009326ee2228d299287c15ae07a827
haeun1700 <haeun7173@gmail.com>
2025-04-22 12:46:09 +0900

    #1: update service작성

--- src/main/java/com/sns/board/service/PostService.java
+++ src/main/java/com/sns/board/service/PostService.java
@@ -1,8 +1,11 @@
 package com.sns.board.service;
 
 import com.sns.board.model.Post;
+import com.sns.board.model.PostPatchRequestBody;
 import com.sns.board.model.PostPostRequestBody;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
+import org.springframework.web.server.ResponseStatusException;
 
 import java.time.ZonedDateTime;
 import java.util.ArrayList;
@@ -22,16 +25,32 @@ public class PostService {
     public List<Post> getPosts() {
         return posts;
     }
+
     public Optional<Post> getPost(Long id) {
-        return posts.stream().filter(post -> id.equals(post.getPostId())).findFirst();
+        return posts.stream().filter(post -> id.equals(post.getId())).findFirst();
     }
 
     public Post createPost(PostPostRequestBody postPostRequestBody) {
-        Long newPostId = posts.stream().mapToLong(Post::getPostId).max().orElse(0L)+1;
+        Long newPostId = posts.stream().mapToLong(Post::getId).max().orElse(0L)+1;
 
         Post newPost = new Post(newPostId, postPostRequestBody.body(), ZonedDateTime.now());
         posts.add(newPost);
 
         return newPost;
+    }
+
+    public Post updatePost(Long id, PostPatchRequestBody postPatchRequestBody) {
+        // id에 해당하는 post가져오기
+        Optional<Post> optionalPost =
+                posts.stream().filter(post -> post.getId().equals(id)).findFirst();
+        // 그 id body값을 받아 다시 그 아이디에 add해주기
+        if(optionalPost.isPresent()){
+            Post updatePost = optionalPost.get();
+            updatePost.setBody(postPatchRequestBody.body());
+
+            return updatePost;
+        } else{
+            throw new ResponseStatusException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다.");
+        }
     }
 }
