76a1792b8b041bcce0c3d83d8b27299cb0dfaa34
haeun1700 <haeun7173@gmail.com>
2025-04-23 14:57:53 +0900

    #2: Post클래스 dto로 전환 - record로 구현

--- src/main/java/com/sns/board/model/Post.java
+++ src/main/java/com/sns/board/model/Post.java
@@ -1,42 +1,20 @@
 package com.sns.board.model;
-
-import lombok.Getter;
-import lombok.Setter;
-
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.sns.board.model.entity.PostEntity;
 import java.time.ZonedDateTime;
-import java.util.Objects;
 
-@Getter
-@Setter
-public class Post{
-    private Long id;
-    private String body;
-    private ZonedDateTime createdAt;
+@JsonInclude(JsonInclude.Include.NON_NULL) //none이 아닐때만 record로 변환해서 반환
+public record Post(
+        Long postId,
+        String body,
+        ZonedDateTime createAt,
+        ZonedDateTime updateAt){
+        public static Post from(PostEntity postEntity){
+            return new Post(
+                    postEntity.getPostId(),
+                    postEntity.getBody(),
+                    postEntity.getCreatedAt(),
+                    postEntity.getUpdatedAt());
+        }
 
-    public Post(long id, String body, ZonedDateTime createdAt) {
-        this.id = id;
-        this.body = body;
-        this.createdAt = createdAt;
-    }
-
-    @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (!(o instanceof Post post)) return false;
-        return Objects.equals(getId(), post.getId()) && Objects.equals(getBody(), post.getBody()) && Objects.equals(getCreatedAt(), post.getCreatedAt());
-    }
-
-    @Override
-    public int hashCode() {
-        return Objects.hash(getId(), getBody(), getCreatedAt());
-    }
-
-    @Override
-    public String toString() {
-        return "Post{" +
-                "id=" + id +
-                ", body='" + body + '\'' +
-                ", createdAt=" + createdAt +
-                '}';
-    }
 }
