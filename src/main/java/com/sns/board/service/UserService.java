718bf301c7fc4136b8376df472c420c3c72edc42
haeun1700 <haeun7173@gmail.com>
2025-04-24 14:54:45 +0900

    #4 - 유저 서비스 구현

---
+++ src/main/java/com/sns/board/service/UserService.java
@@ -0,0 +1,21 @@
+package com.sns.board.service;
+
+import com.sns.board.exception.user.UserNotFoundException;
+import com.sns.board.repository.UserEntityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService implements UserDetailsService {
+    @Autowired
+    UserEntityRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        return userRepository.findByUsername(username).orElseThrow(
+                () -> new UserNotFoundException(username));
+    }
+}
